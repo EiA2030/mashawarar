6cc30211344163dc34cdb393a22f230337d86bef
Eduardo GB <e.gbendito@gmail.com>
2023-05-22 14:42:27 +0000

    update
    commit to main

---
+++ 02_etl/02_01_etl_isda.R
@@ -0,0 +1,130 @@
+get.isda <- function(X = NULL, Y = NULL){
+  dep <- c("top"="0..20cm","bottom"="20..50cm")
+  pars <- c("clay"="clay_tot_psa","sand"="sand_tot_psa","silt"="silt_tot_psa", "texture"="texture.class",
+            "fcc"="fcc","bedrock_cm"="bdr","bulk_density"="db_od","ph"="ph_h2o", "stone_content"="log.wpg2",
+            "carbon_total"="log.c_tot","carbon_organic"="log.oc","nitrogen_total"="log.n_tot_ncs",
+            "phosphorous_extractable"="log.p_mehlich3","potassium_extractable"="log.k_mehlich3",
+            "zinc_extractable"="log.zn_mehlich3","magnesium_extractable"="log.mg_mehlich3","calcium_extractable"="log.ca_mehlich3",
+            "aluminium_extractable"="log.al_mehlich3","iron_extractable"="log.fe_mehlich3","sulphur_extractable"="log.s_mehlich3",
+            "cec"="log.ecec.f")
+  chem <- c("carbon_total"="log.c_tot","carbon_organic"="log.oc",
+            "phosphorous_extractable"="log.p_mehlich3","potassium_extractable"="log.k_mehlich3",
+            "zinc_extractable"="log.zn_mehlich3","magnesium_extractable"="log.mg_mehlich3","calcium_extractable"="log.ca_mehlich3",
+            "aluminium_extractable"="log.al_mehlich3","iron_extractable"="log.fe_mehlich3","sulphur_extractable"="log.s_mehlich3",
+            "cec"="log.ecec.f")
+  url <- "/home/jovyan/common_data/isda/raw/"
+  # aoi <- suppressWarnings(terra::vect(sf::st_as_sf(sf::st_as_sfc(sf::st_bbox(c(xmin = min(X), xmax = max(X), ymax = max(Y), ymin = min(Y)), crs = sf::st_crs(4326))))))
+  outl <- list()
+  soil <- NULL
+  labs <- c("iso", "X", "Y", "depth", "lyr_center")
+  n <- 1
+  for (par in pars) {
+    out <- NULL
+    var <- names(pars[pars == par])
+    for (d in dep) {
+      d <- ifelse(par == "bdr" | par == "fcc", "0..200cm", d)
+      lyr <- paste("sol",par,"m_30m",d,"2001..2017_v0.13_wgs84.tif",sep = "_")
+      tif.cog <- paste0(url,lyr)
+      # data <- suppressWarnings(terra::crop(terra::rast(tif.cog), aoi))
+      data <- suppressWarnings(terra::rast(tif.cog))
+      q <- terra::extract(data, data.frame(x = X, y = Y), xy = TRUE)
+      val <- q[,2]
+      if (par %in% c(chem, "log.wpg2")){val <- expm1(val / 10)}
+      else if (par == "db_od"){val <- val / 100}
+      else if (par == "log.n_tot_ncs"){val <- expm1(val / 100)}
+      else if (par == "ph_h2o"){val <- val / 10}
+      else if (par == "texture.class"){val <- as.character(factor(val, levels = c(1:12), labels = c("Clay", "Silty Clay", "Sandy Clay", "Clay Loam", "Silty Clay Loam", "Sandy Clay Loam", "Loam", "Silt Loam", "Sandy Loam", "Silt", "Loamy Sand", "Sand")))}
+      # Needs to be added
+      else if (par == "fcc"){
+        fcc.atts <- read.table("/home/jovyan/saa-use-case/data/inputs/main/soil/isda/fcc_attributes.tab", sep = "\t", header = T)
+        fcc.atts$Class <- lengths(regmatches(fcc.atts$Description, gregexpr(",", fcc.atts$Description))) + 1
+        fcc.atts$Class <- ifelse(fcc.atts$Description == "No constraints", 0, fcc.atts$Class)
+        fcc.atts$SLPF <- (fcc.atts$Class  - max(fcc.atts$Class))/(min(fcc.atts$Class) - max(fcc.atts$Class))
+        data <- data %% 3000
+        data <- terra::classify(data, cbind(fcc.atts$Value, fcc.atts$SLPF))
+        q <- terra::extract(data, data.frame(x = X, y = Y), xy = TRUE)
+        val <- q[,2]
+        # mx <- terra::minmax(data)[1]
+        # mn <- terra::minmax(data)[2]
+        # val <- (val - mn)/(mx - mn)
+      }
+      else val <- val
+      loc <- paste0("https://nominatim.openstreetmap.org/reverse?lat=",Y,"&lon=",X,"&format=json")
+      loc <- jsonlite::fromJSON(loc)
+      country <- toupper(loc$address$country_code)
+      val <- cbind("ISO" = country, "X" = X, "Y" = Y, "depth" = ifelse(d == "0..20cm", as.integer(20), as.integer(50)), "lyr_center" = ifelse(d == "0..20cm", as.integer(10), as.integer(20+15)), val)
+      colnames(val)[ncol(val)] <- var
+      out <- rbind(out, val)
+    }
+    if(n == 1){
+      soil <- cbind(soil, out)
+    }
+    else {
+      soil <- cbind(soil, out[,6])
+    }
+    labs <- c(labs, var)
+    n <- n + 1
+  }
+  soil <- as.data.frame(soil)
+  colnames(soil) <- labs
+  
+  soil[soil=="NaN"]<-NA
+  
+  return(soil)
+}
+
+isda2dssat <- function(isda = NULL){
+  # Add pedo-transfer values
+  # Drained upper limit (cm3 cm3)
+  isda$DUL <- {
+    clay <- as.numeric(isda$clay) * 1e-2
+    sand <- as.numeric(isda$sand) * 1e-2
+    om <- (as.numeric(isda$carbon_organic) * 1e-2) * 2
+    ans0 <- -0.251 * sand + 0.195 * clay + 0.011 * om + 0.006 * (sand * om) - 0.027 * (clay * om) + 0.452 * (sand * clay) + 0.299
+    ans <- ans0 + (1.283 * ans0^2 - 0.374 * ans0 - 0.015)
+    ans
+  }
+  # Drained upper limit saturated (cm3 cm3)
+  DUL_S <- {
+    clay <- as.numeric(isda$clay) * 1e-2
+    sand <- as.numeric(isda$sand) * 1e-2
+    om <- (as.numeric(isda$carbon_organic) * 1e-2) * 2
+    ans0 <- 0.278 * sand + clay * 0.034 + om * 0.022 + -0.018 * sand * om - 0.027 * clay * om + -0.584 * sand * clay + 0.078
+    ans <- ans0 + (0.636 * ans0 - 0.107)
+    ans
+  }
+  # Lower limit of plant extractable soil water (cm3 cm3)
+  isda$LL15 <- {
+    clay <- as.numeric(isda$clay) * 1e-2
+    sand <- as.numeric(isda$sand) * 1e-2
+    om <- (as.numeric(isda$carbon_organic) * 1e-2) * 2
+    ans0 <- -0.024 * sand + 0.487 * clay + 0.006 * om + 0.005 * sand * om + 0.013 *clay * om + 0.068 *sand * clay +  0.031
+    ans <- ans0 + (0.14 * ans0 - 0.02)
+    ans
+  }
+  # Saturated upper limit (cm3 cm3)
+  isda$SAT <- {
+    sand <- sand * 1e-2
+    ans <- isda$DUL + DUL_S - 0.097 * sand + 0.043
+    ans
+  }
+  # Saturated hydraulic conductivity (cm h1)
+  B <- (log(1500) - log(33))/(log(isda$DUL) - log(isda$LL15))
+  Lambda <- 1/B
+  isda$SKS <- (1930 * (isda$SAT - isda$DUL)^(3 - Lambda)) * 100
+  isda$SSS <- round(as.numeric(isda$SKS), digits = 1)
+  # Albedo (unitless)
+  isda$SALB <- ifelse(isda$texture %in% c("Clay", "Silty Clay", "Silty Clay Loam", "Silt Loam"), 0.12,
+                      ifelse(isda$texture %in% c("Sandy Clay", "Clay Loam", "Sandy Clay Loam", "Loam", "Sandy Loam", "Silt"), 0.13,
+                             ifelse(isda$texture %in% c("Silty Loam"), 0.14,
+                                    ifelse(isda$texture %in% c("Loamy Sand"), 0.16,
+                                           ifelse(isda$texture %in% c("Sand"), 0.19, NA)))))
+  # if(isda$texture %in% c("Clay", "Silty Clay", "Silty Clay Loam", "Silt Loam")){isda$SALB <- 0.12}
+  # else if (isda$texture %in% c("Sandy Clay", "Clay Loam", "Sandy Clay Loam", "Loam", "Sandy Loam", "Silt")){isda$SALB <- 0.13}
+  # else if (isda$texture %in% c("Silty Loam")){isda$SALB <- 0.14}
+  # else if (isda$texture %in% c("Loamy Sand")){isda$SALB <- 0.16}
+  # else if (isda$texture %in% c("Sand")){isda$SALB <- 0.19}
+  # else isda$SALB <- NA
+  isda$SRGF <- 1*exp(-0.02 * as.numeric(isda$lyr_center))
+  return(isda)
+}
---
+++ 0_saa.R
@@ -0,0 +1,102 @@
+# 1. Read inputs and check validity
+  # 1.1 Catch arguments from bash
+args <- commandArgs()
+aoi <- as.character(args[6])
+start.Date <- as.Date(args[7])
+end.Date <- as.Date(args[8])
+workers <- as.integer(args[9])
+  # 1.2 Check validity of inputs and necessary files
+    # Check the spatial inputs (either file or a valid state in Nigeria)
+if(grepl(paste0("v", format(Sys.Date(), "%Y%m%d"), ".csv"), aoi)){
+  if(!file.exists(paste0("/home/jovyan/saa-use-case/data/inputs/user/", "v", format(Sys.Date(), "%Y%m%d"), ".csv"))){
+    stop("Please, ensure that a valid file with coordinates (Longitude and latitude columns, in .csv) is located in the user folder.")
+  }
+} else {
+  if(!(aoi %in% c("Kano", "Kaduna"))){
+    stop("Please, use an accepted state in Nigeria for the DST")
+  }
+}
+    # Check if dates are correct and valid
+if(as.Date(start.Date) < as.Date("1990-01-01") | as.Date(start.Date) > as.Date(paste0(as.integer(format(Sys.Date(), "%Y"))-1, "-12-31"))){
+  stop("Start date is not accepted. Please, make sure is after or on 1st Jan 1990 and not grater than Dec 31st of last year. Also ensure is the correct date format (YYYY-MM-DD)")
+}
+if(as.Date(end.Date) < as.Date("1990-01-01") | as.Date(end.Date) > as.Date(paste0(as.integer(format(Sys.Date(), "%Y"))-1, "-12-31")) | as.Date(end.Date) < as.Date(start.Date)){
+  stop("End date is not accepted. Please, make sure is after or on 1st Jan 1990, not grater than Dec 31st of last year and after the *Start Date*. Also ensure is the correct date format (YYYY-MM-DD)")
+}
+     # Check that the number of jobs is appropriate
+if(workers > 12){stop("Number of parallel workers not accepted. Please use a number lower than 12")}
+    # Check that the necessary .MZX template file is there
+if(!file.exists(paste0("/home/jovyan/saa-use-case/data/inputs/dssat/xfiles/v", as.character(format(Sys.Date(), "%Y%m%d")), ".MZX"))) stop("Please, add an .MZX template")
+# # 2. Install requirements
+# packs <- data.frame("package" = c("tidyverse", "lubridate", "DSSAT", "doParallel", "foreach"),
+#                     "version" = c("1.3.2", "1.9.2", "0.0.6", "1.0.17", "1.5.2"))
+# ipacks <- installed.packages()
+# for (p in 1:nrow(packs)){
+#   pack <- packs[p,1]
+#   ver <- packs[p,2]
+#   print(ipacks[ipacks[,"Package"] == pack, "Version"])
+#   # if(pack %in% ipacks[,"Package"] == FALSE){
+#   #   install_version(pack, ver, repos = "https://cloud.r-project.org/")
+#   # }
+#   # else if(ver != ipacks[ipacks[,"Package"] == pack, "Version"]){
+#   #   utils::remove.packages(pack)
+#   #   install_version(pack, ver, repos = "https://cloud.r-project.org/")
+#   # }
+# }
+# 3. Source Functions
+funcs <- list.files("/home/jovyan/saa-use-case/functions",
+                        recursive = TRUE, full.names = TRUE, include.dirs = FALSE,
+                        pattern = paste0(c("02", "03", "04"), collapse = '|'))
+for (func in funcs) {
+  source(func)
+}
+# 4. Start DST
+  # 4.1 Define the AOI depending on the input by the user
+if(grepl(paste0("v", format(Sys.Date(), "%Y%m%d"), ".csv"), aoi)){
+  gps <- read.csv(paste0("/home/jovyan/saa-use-case/data/inputs/user/", "v", format(Sys.Date(), "%Y%m%d"), ".csv"))
+} else {
+  gps <- define.regions(iso = "NGA", level = 1, resolution = 0.05)
+  gps <- gps[gps$NAME_1 == aoi,]
+}
+  # 4.2 Define the list of years to simulate
+years <- unique(format(seq(as.Date(as.character(start.Date)), as.Date(as.character(end.Date)), by = "day"), "%Y"))
+  # 4.3 Create the intermediate directory for DSSAT files
+dssat.paths <- paste0("/home/jovyan/saa-use-case/data/intermediate/dssat/v", as.character(format(Sys.Date(), "%Y%m%d")), "/", years)
+for (dssat.path in dssat.paths) {
+  dir.create(dssat.path, recursive = TRUE)
+}
+  # 4.4 DSSAT
+  # 4.4.0 Remove all pre-existing varieties. Replace with ours
+CUL <- DSSAT::read_cul("/home/jovyan/saa-use-case/data/inputs/dssat/MZCER047.CUL")
+DSSAT::write_cul(CUL, file_name = "/opt/DSSAT/Genotype/MZCER047.CUL")
+# Start simulations
+for (year in years) {
+  dssat.intermediate <- paste0("/home/jovyan/saa-use-case/data/intermediate/dssat/v", as.character(format(Sys.Date(), "%Y%m%d")), "/", year)
+    # 4.4.1 Execute DSSAT + ETL
+  dssat.extdata(coords = gps,
+                sdate = paste0(year, "-01-01"),
+                edate = paste0(year, "-12-31"),
+                jobs = workers,
+                path.to.ex = dssat.intermediate)
+    # 4.4.2 Define experiment file
+  dssat.Xdata(coords = gps,
+              sdate = paste0(year, "-01-01"),
+              edate = paste0(year, "-12-31"),
+              jobs = workers,
+              path.to.ex = dssat.intermediate)
+    # 4.4.3 Execute DSSAT
+  dssat.execute(coords = gps,
+                sdate = paste0(year, "-01-01"),
+                edate = paste0(year, "-12-31"),
+                jobs = workers,
+                path.to.ex = dssat.intermediate)
+}
+  # 4.5 Aggregation
+    # 4.5.1 Aggregate DSSAT outputs by year
+dssat.aggregate(years = years,
+                jobs = workers,
+                path.to.ex = paste0("/home/jovyan/saa-use-case/data/intermediate/dssat/v", as.character(format(Sys.Date(), "%Y%m%d"))))
+    # 4.5.2 Ranking the final results
+rank.aggregate(years = years,
+               jobs = workers,
+               path.to.ex = paste0("/home/jovyan/saa-use-case/data/intermediate/dssat/v", as.character(format(Sys.Date(), "%Y%m%d"))))
